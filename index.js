527df6933f8ec1a5838766746f5c1e6befde3fd1
Fellipe <83995870+fellipe-fk@users.noreply.github.com>
2023-09-21 15:17:57 -0300

    aquirvos

---
+++ index.js
@@ -0,0 +1,57 @@
+import express from 'express';
+import {retornaColecaoHistoricoInflacao, buscaAnoHistoricoInflacao, buscaIdHistoricoInflacao,calcularReajuste,validacaoErro} from './funcoes/servico.js';
+
+const app = express();
+
+app.get('/historicoIPCA/calculo', (req, res) => {
+
+    const valor = parseFloat(req.query.valor);
+    const dataInicialMes = parseInt(req.query.mesInicial);
+    const dataInicialAno = parseInt(req.query.anoInicial);
+    const dataFinalMes = parseInt(req.query.mesFinal);
+    const dataFinalAno = parseInt(req.query.anoFinal); 
+
+    if (validacaoErro(valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno)) {
+        res.status(400).json({ erro: 'Parâmetros inválidos' });
+        return;
+      }
+    
+      const resultado = calcularReajuste(valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno);
+      res.json({ resultado: resultado });
+});
+
+
+app.get('/historicoIPCA/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if(isNaN(id)){
+        res.status(404).json({ erro: 'ID inválido' });
+        return;
+    };
+
+    const idResultado = buscaIdHistoricoInflacao(id);
+    if(idResultado){
+        res.json(idResultado);
+    }else{
+        res.status(404).json({erro:'Id nao encontrado'})
+    };
+});
+
+
+app.get('/historicoIPCA', (req, res) => {
+    const ano = parseInt(req.query.ano);
+    if(isNaN(ano)){
+        res.status(404).json({erro: 'valor passado invalido'});
+    }else{
+        const filtraAno = buscaAnoHistoricoInflacao(ano);
+        if (filtraAno.length > 0) {
+            res.json(filtraAno);
+          } else {
+            res.status(404).json({ erro: "Nenhuma Ano foi encontrado" });
+          };
+    }
+});
+
+app.listen(8080, () => {
+    console.log('Api de IPA inicializando..');
+});
---
+++ servico.js
@@ -0,0 +1,64 @@
+import historicoInflacao from '../dados/dados.js';
+
+export const retornaColecaoHistoricoInflacao = () => {
+    return historicoInflacao;
+};
+
+export const buscaAnoHistoricoInflacao = (buscaAno) => {
+    const filtraAno = historicoInflacao.filter(item => item.ano === buscaAno);
+    return filtraAno;
+};
+
+export const buscaIdHistoricoInflacao = (id) => {
+    const filtraId = parseInt(id)
+    const historico = historicoInflacao.find(item => item.id === filtraId);
+    return historico;
+};
+
+export const calcularReajuste = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
+    const historicoFiltrado = historicoInflacao.filter(
+      historico => {
+        if (dataInicialAno === dataFinalAno) {
+          return historico.ano === dataInicialAno && historico.mes >= dataInicialMes && historico.mes <= dataFinalMes;
+        } else {
+          return (
+            (historico.ano === dataInicialAno && historico.mes >= dataInicialMes) ||
+            (historico.ano > dataInicialAno && historico.ano < dataFinalAno) ||
+            (historico.ano === dataFinalAno && historico.mes <= dataFinalMes)
+          );
+        }
+      }
+    );
+  
+    let taxasMensais = 1;
+    for (const elemento of historicoFiltrado) {
+      taxasMensais *= (elemento.ipca / 100) + 1;
+    }
+  
+    const resultado = valor * taxasMensais;
+    return parseFloat(resultado.toFixed(2));
+  };
+  
+  export const validacaoErro = (valor, dataInicialMes, dataInicialAno, dataFinalMes, dataFinalAno) => {
+    const anoLimiteFinal = historicoInflacao[historicoInflacao.length - 1].ano;
+    const anoLimiteInicial = historicoInflacao[0].ano
+    const mesLimiteFinal = historicoInflacao[historicoInflacao.length - 1].mes;
+    if (
+      isNaN(valor) ||
+      isNaN(dataInicialMes) ||
+      isNaN(dataInicialAno) ||
+      isNaN(dataFinalMes) ||
+      isNaN(dataFinalAno) ||
+      dataInicialMes < 1 || dataInicialMes > 12 ||
+      dataInicialAno < anoLimiteInicial || dataInicialAno > anoLimiteFinal ||
+      dataFinalMes < 1 || dataFinalMes > 12 ||
+      dataFinalAno < anoLimiteInicial || dataFinalAno > anoLimiteFinal ||
+      (dataFinalAno === anoLimiteFinal && dataFinalMes > mesLimiteFinal) ||
+      dataFinalAno < dataInicialAno ||
+      (dataFinalAno == dataInicialAno && dataFinalMes < dataInicialMes)
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
